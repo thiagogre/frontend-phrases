ce49b47fcd69a03a4c5d092dfe170f8d8b3fc93f
thiago <thiagoluiz_16@hotmail.com>
2020-10-31 16:59:35 -0300

    feat: add phrases iteration

--- src/components/Phrases.js
+++ src/components/Phrases.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import { For } from 'react-loops'
 
 
 export const Phrases = () => {
@@ -29,11 +30,15 @@ export const Phrases = () => {
       <div className='content'>
         <div className='english'>
           <img src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/emojidex/112/flag-for-united-states_1f1fa-1f1f8.png'} alt='USA' />
-          <p>Phrase</p>
+          <For of={phrases} as={phrase =>
+            <p><em>{phrase}</em></p>
+          } />
         </div>
         <div className='portuguese'>
           <img src={'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/emojidex/112/flag-for-brazil_1f1e7-1f1f7.png'} alt='BRA' />
-          <p>Frase</p>
+          <For of={frases} as={frase =>
+            <p>{frase}</p>
+          } />
         </div>
       </div>
     </div>
